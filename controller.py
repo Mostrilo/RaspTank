bb4ddffc369afbfd64662d29c6314e7692758024
Alexander Mertens <57722542+Mesalcode@users.noreply.github.com>
2020-09-27 17:20:52 +0200

    Proper formatting

--- controller.py
+++ controller.py
@@ -5,20 +5,20 @@ import time
 import random
 
 def turn_left(degrees):
-  movement.moveTank(100,100)
-  time.sleep(.008888*degrees)
+  movement.moveTank(100, 100)
+  time.sleep(.008888 * degrees)
   movement.motorStop()
 
 def turn_right(degrees):
-  movement.moveTank(-100,-100)
-  time.sleep(.008888*degrees)
+  movement.moveTank(-100, -100)
+  time.sleep(.008888 * degrees)
   movement.motorStop()
 
 def move_forward(seconds):
-   movement.moveTank(-100,100)
+   movement.moveTank(-100, 100)
    time.sleep(seconds)
 
 def move_backwards(seconds):
-   movement.moveTank(100,-100)
+   movement.moveTank(100, -100)
    time.sleep(seconds)
 
