d524689879270b1d6b3a6437cf73f7341dcb3edf
Alexander Mertens <57722542+Mesalcode@users.noreply.github.com>
2020-09-27 18:05:14 +0200

    changed test file

--- server.py
+++ server.py
@@ -6,10 +6,12 @@ import controller
 
 start = time.time()
 while True:
+    open_hand()
     while ultrasonic.distance() > 0.4:
         #print(ultrasonic.distance())
-        controller.move_forward(0.05)
+        controller.move_forward(0.01)
     left = bool(random.getrandbits(1))
+    close_hand()
     if(left):
         controller.turn_left(90)
     else:
