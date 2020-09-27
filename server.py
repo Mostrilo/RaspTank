4bdb150c86123a578fab4bd0a3c2a15879e25a81
Alexander Mertens <57722542+Mesalcode@users.noreply.github.com>
2020-09-27 12:40:23 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import movement
+import ultrasonic
 import time
 
 movement.moveArcade(10, 20)
@@ -6,3 +7,5 @@ movement.moveArcade(10, 20)
 time.sleep(1)
 
 movement.moveTank(10, 10)
+
+print(ultrasonic.distance())
