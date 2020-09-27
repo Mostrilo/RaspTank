93dcb5c3d860ecbb67f55430e65c61a514bf65a0
Alexander Mertens <57722542+Mesalcode@users.noreply.github.com>
2020-09-27 17:26:52 +0200

    More functionality regarding servos

--- controller.py
+++ controller.py
@@ -3,6 +3,7 @@ import movement
 import ultrasonic 
 import time 
 import random
+import servos
 
 def turn_left(degrees):
   movement.moveTank(100, 100)
@@ -21,4 +22,21 @@ def move_forward(seconds):
 def move_backwards(seconds):
    movement.moveTank(100, -100)
    time.sleep(seconds)
+
+  
+def open_hand(degrees=-10):
+  servos.hand(degrees)
+  time.sleep(.15)
+  
+def close_hand(degrees=7):
+  servos.hand(degrees)
+
+def move_arm(position):
+  servos.arm_pos(position)
+  time.sleep(.2)
+  
+def move_wrist(degrees):
+  #-90 to 90 degrees
+  servos.wrist(int((degrees/12)+6))
+  time.sleep(.25)
 
