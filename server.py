eca1b47bd654843864e20cfa3aa5d7747eb21237
=Alexander Mertens <=alexandermertens06@gmail.com>
2020-10-11 18:39:58 +0200

    Changes

--- server.py
+++ server.py
@@ -1,31 +1,34 @@
 #import movement 
-#import ultrasonic 
+import ultrasonic 
 #import time 
-#import random
-#import controller
-import speakers
+import random
+import controller
+#import speakers
 
 #start = time.time()
-"""
+
 while True:
     controller.open_hand()
     while ultrasonic.distance() > 0.4:
         #print(ultrasonic.distance())
         controller.move_forward(0.01)
     #left = bool(random.getrandbits(1))
-    left = True
+    left = bool(random.getrandbits(1))
     controller.close_hand()
     if(left):
-        controller.turn_left(90)
+        while ultrasonic.distance() <= 0.7:
+            controller.turn_left(130)
     else:
-        controller.turn_right(90)
-"""
-"""
-controller.turn_right(90)
+        while ultrasonic.distance() <= 0.7:
+            controller.turn_right(130)
+
+#controller.turn_right(130)
 #controller.turn_right(90)
-"""
+#
+
+#controller.turn_right(90)
 #movement.destroy()
 #speakers.
-controller.turn_right(90)
 #controller.turn_right(90)
+#controller.turn_left(90)
 
